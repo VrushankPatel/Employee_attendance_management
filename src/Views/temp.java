4b79aad0219f7dbc5bb578dd47a199dc3a771076
Vrushank Patel <vrushankpatel5@gmail.com>
2019-11-16 13:17:37 +0530

    Additional UI related changes to Login Panel

--- src/Views/temp.java
+++ src/Views/temp.java
@@ -1,11 +1,10 @@
 package Views;
 import java.io.*;
-import org.json.*;  
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*; 
 
 public class temp {
-    public static void main(String[] args) throws JSONException, FileNotFoundException, IOException, ParseException {
+    public static void main(String[] args){
         try {
             JSONObject jsonObject = (JSONObject) new JSONParser().parse(new FileReader("Properties.json"));                         
             System.out.println("Name: " + jsonObject.get("firstName").toString());
