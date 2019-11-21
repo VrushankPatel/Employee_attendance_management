448743d4fe47cc76d52ba207477840035209db03
Vrushank Patel <vrushankpatel5@gmail.com>
2019-11-21 15:39:15 +0530

    created encryption engine with cryptography, decrypter, encrypter and MD5Encoder.

--- SQL Schema/Sql_schema_latest.sql
+++ SQL Schema/Sql_schema_latest.sql
@@ -1,6 +1,6 @@
-create table Administrator(Company_id int NOT NULL auto_increment,Admin_username varchar(255),Admin_Password text NOT NULL,PRIMARY KEY(Company_id)) ENGINE=INNODB;
+create table Administrator(Company_id int NOT NULL auto_increment,Admin_username varchar(255),Admin_Password longtext NOT NULL,PRIMARY KEY(Company_id)) ENGINE=INNODB;
 ALTER TABLE `Administrator` AUTO_INCREMENT=0;
-create table Employee(Employee_Company_id int NOT NULL,Employee_id int NOT NULL,Emp_full_name varchar(255),Emp_address text,Emp_phone bigint,primary key(Employee_Company_id,Employee_id),FOREIGN KEY (Employee_Company_id) REFERENCES Administrator(Company_id) ON DELETE CASCADE)ENGINE=INNODB;
+create table Employee(Employee_Company_id int NOT NULL,Employee_id int NOT NULL,Emp_full_name varchar(255),Emp_address longtext,Emp_phone bigint,primary key(Employee_Company_id,Employee_id),FOREIGN KEY (Employee_Company_id) REFERENCES Administrator(Company_id) ON DELETE CASCADE)ENGINE=INNODB;
 CREATE TABLE Attendance(Attendance_Employee_Company_id int NOT NULL,Attendance_Employee_id int NOT NULL,attendance_date Date,attendance_status char NOT NULL,primary key(Attendance_Employee_Company_id,Attendance_Employee_id),FOREIGN KEY (Attendance_Employee_Company_id,Attendance_Employee_id) REFERENCES Employee(Employee_Company_id,Employee_id) ON DELETE CASCADE)ENGINE=INNODB;
 
 
---
+++ src/EncryptionEngine/MessageDigestEncoder.java
@@ -0,0 +1,21 @@
+package EncryptionEngine;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+public class MessageDigestEncoder {
+    MessageDigest m;
+    public MessageDigestEncoder() throws NoSuchAlgorithmException {
+        m=MessageDigest.getInstance("MD5");
+    }    
+    public String md5(String get){
+        try{                            
+            m.update(get.getBytes(),0,get.length());
+            //System.out.println("MD5: "+new BigInteger(1,m.digest()).toString(16));
+            return new BigInteger(1,m.digest()).toString(16);
+        }
+        catch(Exception e){
+            return "";
+        }
+    }
+}
